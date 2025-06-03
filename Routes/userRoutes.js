c6cd4d0ddbfeefae75327b998a444e23cfcb76ea
Jaffar43 <mr.jaffarali2001@gmail.com>
2025-06-03 14:29:49 +0300

    Cearte user routes and add the router

---
+++ Routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const {
+  Register,
+  Login,
+  UpdatePassword,
+  CheckSession
+} = require('../controllers/users')
+
+router.post('/register', Register)
+router.post('/login', Login)
+router.put('/:id', UpdatePassword)
+router.get('/session', CheckSession)
+
+module.exports = router
