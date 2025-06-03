6dc1380d2b9ca678a736723b432627ac2bcdcc45
shahnazalqassab <s.alqassab@gmail.com>
2025-06-03 14:17:43 +0300

    adding the server.js & database

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+
+const userRouter = require('./Routers/userRouter')
+const courseRouter = require('./Routers/courseRoutes')
+
+const PORT = process.env.PORT || 3001
+
+const DB = require('./Database')
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/user', userRouter)
+app.use('/courses', courseRouter)
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
