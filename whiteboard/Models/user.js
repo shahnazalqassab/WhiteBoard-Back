147da1da1e91ee6263374a8cd9995dd87ec14499
shahnazalqassab <s.alqassab@gmail.com>
2025-06-03 10:06:01 +0300

    models created

---
+++ whiteboard/Models/user.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    category: { type: String, enum: ['student', 'teacher']},
+  },
+  { timestamps: true }
+)
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
